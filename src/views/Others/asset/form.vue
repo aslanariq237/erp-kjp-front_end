066b156353a9775748bfeecc4ad39748c20acef2
aslanariq237 <aslanariq009@gmail.com>
2025-02-18 00:17:02 +0700

    front-end ERP KJP

---
+++ src/views/Others/asset/form.vue
@@ -0,0 +1,175 @@
+<template>
+    <AdminLayout>
+        <Form @submit="onSubmit">
+            <div class="">
+                <div class="flex justify-between items-center shadow-lg rounded-lg p-4">
+                    <div class="card">
+                        <p class="text-xl font-semibold">Form Assets</p>
+                        <p class="text-gray-400">Master Data / Asset / Form</p>
+                    </div>
+                    <div class="btn flex justify-between items-center gap-4 text-xl">
+                        <RouterLink to="/asset"
+                            class=" px-3 py-3 my-2bg-blue-500 rounded-md cursor-pointer">Cancel
+                        </RouterLink>
+                        <div class="btn">
+                            <button type="submit" class="bg-green-400 px-3 py-2 rounded-md text-white">Submit</button>
+                        </div>
+                    </div>
+                </div>                
+                <div class="input shadow-lg rounded-4 shadow=md p-4 mt-4">                    
+                    <div class="field mt-3 text-lg font-semibold">
+                        <div class="flex justify-between align-top gap-5">
+                            <div class="name w-full">
+                                <label>Asset Name</label>
+                                <input 
+                                    type="text" 
+                                    id="name_asset" 
+                                    name="name_asset" 
+                                    class="w-full rounded-md px-3 py-3 my-2"
+                                    placeholder="Masukkan Nama Asset" 
+                                    v-model="name"
+                                >
+                                <div class="fv-plugins-message-container">
+                                    <div class="fv-help-block">
+                                        <p class="text-red-400 text-md italic" 
+                                        v-if="rules.name == true">
+                                            Asset Name is required
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="asset-type w-full">
+                                <label>Asset Type</label>
+                                <select 
+                                    name="asset_type" 
+                                    id="asset_type" 
+                                    class="w-full rounded-md px-3 py-3 my-2"
+                                    v-model="id_asset_type">
+                                    <option value="">-- Select type -- </option>
+                                    <option value="abc">Abc</option>
+                                </select>
+                                <div class="fv-plugins-message-container">
+                                    <div class="fv-help-block">
+                                        <p class="text-red-400 text-md italic" v-if="rules.id_asset_type == true">
+                                            Asset Type is required
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="flex justify-between gap-5 align-top mt-3">
+                            <div class="name w-full">
+                                <label>Quantity</label>
+                                <input 
+                                    type="text" 
+                                    id="quantity_asset" 
+                                    name="quantity_asset"
+                                    class="w-full rounded-md px-3 py-3 my-2" 
+                                    placeholder="Masukkan quantity Asset"
+                                    v-model="qty"
+                                >
+                                <div class="fv-plugins-message-container">
+                                    <div class="fv-help-block">
+                                        <p class="text-red-400 text-md italic" 
+                                        v-if="rules.qty == true">
+                                            Quantity is required
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="status w-full">
+                                <label>Status</label>
+                                <input 
+                                    type="text" 
+                                    id="status_asset" 
+                                    name="status_asset"
+                                    class="w-full rounded-md px-3 py-3 my-2" 
+                                    placeholder="Masukkan Status Asset"
+                                    v-model="status">
+                                <div class="fv-plugins-message-container">
+                                    <div class="fv-help-block">
+                                        <p class="text-red-400 text-md italic" 
+                                        v-if="rules.status == true">
+                                            Status is required
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>                        
+                    </div>
+                </div>
+            </div>
+        </Form>
+    </AdminLayout>
+</template>
+<script>
+import AdminLayout from '@/components/layout/AdminLayout.vue';
+import { Form, Field, ErrorMessage } from "vee-validate";
+import { RouterLink } from 'vue-router';
+
+
+export default {
+    name: "assets-forms",
+    components: {
+        AdminLayout,
+        Form,
+    },
+    data() {
+        return {
+            id_asset_type: "",
+            name: "",            
+            qty: null,
+            status: "",
+            rules: {
+                asset_type: false,
+                name: false,                
+                qty: false,
+                status: false,
+            }
+        }
+    },
+    methods: {
+        validation() {
+            var count = 0;
+
+            if (this.name == "" || this.name == null) {
+                this.rules.name = true;
+                count++;
+            } else {
+                this.rules.name = false;
+            } 
+            
+            if (this.id_asset_type == '' || this.id_asset_type == null ) {
+                this.rules.asset_type = true;
+                count++;
+            } else {
+                this.rules.asset_type = false;
+            } 
+
+            if (this.qty == "" ||this.qty == null) {
+                this.rules.qty = true;
+                count++;
+            } else {
+                this.rules.qty = false;
+            }
+
+            if (this.status == "" || this.status == null) {
+                this.rules.status = true;
+                count++;
+            } else {
+                this.rules.status = false;
+            }
+        },
+        async onSubmit() {            
+            const validate = this.validation();
+            const data = [
+                this.id_asset_type,
+                this.name,                
+                this.qty,
+                this.status
+            ]
+            console.log(data);
+        }
+    }
+}
+</script>
---
+++ src/views/Others/employee/form.vue
@@ -0,0 +1,238 @@
+<template>
+    <AdminLayout>
+        <Form @submit="onSubmit">
+            <div class="">
+                <div class="flex justify-between items-center shadow-lg rounded-lg p-4">
+                    <div class="card">
+                        <p class="text-xl font-semibold">Form Employee</p>
+                        <p class="text-gray-400">Master Data - Employee - Form</p>
+                    </div>
+                    <div class="btn flex justify-between items-center gap-5">
+                        <div class="btn">
+                            <RouterLink to="/employee" class="px-5 py-2 text-xl cursor-pointer ">Cancel</RouterLink>
+                        </div>
+                        <div class="btn">
+                            <button type="submit" class="text-xl bg-green-400 px-5 py-2 rounded-md text-white">Submit</button>
+                        </div>
+                    </div>
+                </div>                
+                <div class="input shadow-lg rounded-lg p-4 mt-4">
+                    <div class="field mt-5 text-lg font-semibold text-gray-800">
+                        <div class="flex justify-between align-top gap-5">
+                            <div class="employee-name w-full">
+                                <label>Employee Name<label class="text-red-500">*</label></label>
+                                <input 
+                                    type="text"
+                                    id="employee_name"
+                                    name="employee_name"
+                                    class="w-full rounded-md px-3 py-3 my-2"                                    
+                                    placeholder="Insert Employee Name"
+                                    v-model="employee_name"
+                                >
+                                <div class="fv-plugins-message-container">
+                                    <div class="fv-help-block">
+                                        <p class="text-red-400 text-md italic" 
+                                        v-if="rules.employee_name == true">
+                                            Employee Name is required
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="employee-phone w-full">
+                                <label>Employee Phone<label class="text-red-500">*</label></label>
+                                <input 
+                                    type="text"
+                                    id="employee_phone"
+                                    name="employee_phone"
+                                    class="w-full rounded-md px-3 py-3 my-2"
+                                    placeholder="Insert Employee Phone"
+                                    v-model="employee_phone"
+                                >
+                                <div class="fv-plugins-message-container">
+                                    <div class="fv-help-block">
+                                        <p class="text-red-400 text-md italic" 
+                                        v-if="rules.employee_phone == true">
+                                            Employee Phone is required
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>                           
+                        </div>
+                        <div class="flex justify-between gap-5 align-top mt-3">
+                            <div class="employee-email w-full">
+                                <label>Email<label class="text-red-500">*</label></label>
+                                <input 
+                                    type="text"
+                                    id="employee_email"
+                                    name="employee_email"
+                                    class="w-full rounded-md px-3 py-3 my-2"
+                                    placeholder="Masukkan employee Email"
+                                    v-model="employee_email"
+                                >
+                                <div class="fv-plugins-message-container">
+                                    <div class="fv-help-block">
+                                        <p class="text-red-400 text-md italic" 
+                                        v-if="rules.employee_email == true">
+                                            Employee Email is required
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="employee-address w-full">
+                                <label>Employee Address<label class="text-red-500">*</label></label>
+                                <input 
+                                    type="text"
+                                    id="employee_address"
+                                    name="employee_address"
+                                    class="w-full rounded-md px-3 py-3 my-2"
+                                    placeholder="Masukkan employee Address"
+                                    v-model="employee_address"
+                                >
+                                <div class="fv-plugins-message-container">
+                                    <div class="fv-help-block">
+                                        <p class="text-red-400 text-md italic" 
+                                        v-if="rules.employee_address == true">
+                                            Employee Address is required
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="flex justify-between gap-5 align-top mt-3">
+                            <div class="NIK w-full">
+                                <label>NIK<label class="text-red-500">*</label></label>
+                                <input 
+                                    type="text"
+                                    id="nik"
+                                    name="nik"
+                                    class="w-full rounded-md px-3 py-3 my-2"
+                                    placeholder="Masukkan NIK"
+                                    v-model="NIK"
+                                >
+                                <div class="fv-plugins-message-container">
+                                    <div class="fv-help-block">
+                                        <p class="text-red-400 text-md italic" 
+                                        v-if="rules.NIK == true">
+                                            NIK is required
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="position w-full">
+                                <label>Position<label class="text-red-500">*</label></label>
+                                <input 
+                                    type="text"
+                                    id="position"
+                                    name="position"
+                                    class="w-full rounded-md px-3 py-3 my-2"
+                                    placeholder="Masukkan position"
+                                    v-model="position"
+                                >
+                                <div class="fv-plugins-message-container">
+                                    <div class="fv-help-block">
+                                        <p class="text-red-400 text-md italic" 
+                                        v-if="rules.position == true">
+                                            Position is required
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </Form>
+    </AdminLayout>
+</template>
+<script>
+import AdminLayout from '@/components/layout/AdminLayout.vue';
+import { Form, Field, ErrorMessage } from "vee-validate";
+import { RouterLink } from 'vue-router';
+
+
+export default {
+    name: "assets-forms",
+    components: {
+        AdminLayout, 
+        Form,       
+    },
+    data(){
+        return{ 
+            id_employee : "",
+            employee_name : "",
+            employee_phone : null,
+            employee_email : "",
+            employee_address : "",
+            NIK : "",
+            position: "",
+            rules : {
+                employee_name : false,
+                employee_phone : false,
+                employee_email : false,
+                employee_address : false,
+                NIK : false,
+                position : false
+            }
+        }
+    },
+    methods: {
+        validation(){
+            var count = 0;
+            if (this.employee_name == "" || this.employee_name == null) {
+                this.rules.employee_name = true;
+                count++;
+            }else{
+                this.rules.employee_name = false;
+            }
+
+            if (this.employee_phone == "" || this.employee_phone == null) {
+                this.rules.employee_phone = true;
+                count++;
+            }else{
+                this.rules.employee_phone = false
+            }
+
+            if (this.employee_email == "" || this.employee_email == null) {
+                this.rules.employee_email = true;
+                count++;
+            }else{
+                this.rules.employee_email = false;
+            }
+            if (this.employee_address == "" || this.employee_address == null) {
+                this.rules.employee_address = true;
+                count++;
+            }else{
+                this.rules.employee_address = false;
+            }
+
+            if (this.NIK == "" || this.NIK == null) {
+                this.rules.NIK = true;
+                count++;
+            }else{
+                this.rules.NIK = false;
+            }
+
+            if (this.position == "" || this.position == null) {
+                this.rules.position = true;
+                count++
+            }else{
+                this.rules.position = false;
+            }
+        },
+        async onSubmit(){
+            const data = [
+                this.id_employee,
+                this.employee_name,
+                this.employee_phone,
+                this.employee_email,
+                this.employee_address,
+                this.NIK,
+                this.position
+            ]
+
+            const result = await this.validation();
+            console.log(data);  
+        }
+    }
+}
+</script>
