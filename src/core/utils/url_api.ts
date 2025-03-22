0c6ffd7c2843b2bb7abb9df970e3231fd966c772
Aslan <60170799+aslanariq237@users.noreply.github.com>
2025-03-22 07:54:42 +0700

    newly repo local

--- src/core/utils/url_api.ts
+++ src/core/utils/url_api.ts
@@ -1,73 +1,73 @@
-let baseUrl = ""
+let baseUrl = '';
 
-baseUrl = import.meta.env.VITE_APP_API_URL + "/api/"
+baseUrl = import.meta.env.VITE_APP_API_URL + '/api/'
 
 //A
-export const AccReceive = baseUrl + "account_receivable";
-export const AccPayable = baseUrl + "account_payable";
-export const Asset = baseUrl + "assets";
-export const AddAsset = baseUrl + "assets_code";
+export const AccReceive = baseUrl + 'account_receivable';
+export const AccPayable = baseUrl + 'account_payable';
+export const Asset = baseUrl + 'assets';
+export const AddAsset = baseUrl + 'assets_code';
 //B
-export const BankAccount = baseUrl + "bank_accounts"
+export const BankAccount = baseUrl + 'bank_accounts'
 //C
-export const Customer = baseUrl + "customers"
-export const GetCustomer = baseUrl + "customers"
-export const AddCustomer = baseUrl + "store-customers"
-export const CustomerCode = baseUrl + "customers_code"
+export const Customer = baseUrl + 'customers'
+export const GetCustomer = baseUrl + 'customers'
+export const AddCustomer = baseUrl + 'store-customers'
+export const CustomerCode = baseUrl + 'customers_code'
 
 //D
-export const DeliveryOrder = baseUrl + "delivery_orders";
-export const DeliverSales = baseUrl + "delivery_sales";
-export const AddDeliveryOrder = baseUrl + "store-do";
-export const DetailPo = baseUrl + "detail_po";
-export const DetailSo = baseUrl + "details_so";
-export const DetailDo = baseUrl + "detail_do";
-export const DetailQuatation = baseUrl + "detail_quatation";
-export const DetailTandater = baseUrl + "detail_tandater";
+export const DeliveryOrder = baseUrl + 'delivery_orders';
+export const DeliverSales = baseUrl + 'delivery_sales';
+export const AddDeliveryOrder = baseUrl + 'store-do';
+export const DetailPo = baseUrl + 'detail_po';
+export const DetailSo = baseUrl + 'details_so';
+export const DetailDo = baseUrl + 'detail_do';
+export const DetailQuatation = baseUrl + 'detail_quatation';
+export const DetailTandater = baseUrl + 'detail_tandater';
 
 //E
-export const Employee = baseUrl + "employees"
-export const EmployeeCode = baseUrl + "employees_code"
+export const Employee = baseUrl + 'employees'
+export const EmployeeCode = baseUrl + 'employees_code'
 
 //F
-export const fileUpload = baseUrl + "file-upload";
-export const file = baseUrl + "file";
-export const fileDoc = baseUrl + "documents";
+export const fileUpload = baseUrl + 'file-upload';
+export const file = baseUrl + 'file';
+export const fileDoc = baseUrl + 'documents';
 //I
-export const Invoice = baseUrl + "invoices"
-export const DetailInvoice = baseUrl + "detail_invoices"
-export const InvoiceAdd = baseUrl + "invoices_code"
+export const Invoice = baseUrl + 'invoices'
+export const DetailInvoice = baseUrl + 'detail_invoices'
+export const InvoiceAdd = baseUrl + 'invoices_code'
 
-export const Inquiry = baseUrl + "inquiry"
-export const InquiryAdd = baseUrl + "inquiry_code"
+export const Inquiry = baseUrl + 'inquiry'
+export const InquiryAdd = baseUrl + 'inquiry_code'
 
 //L
-export const LaporUang = baseUrl + "laporan_keuangan"
-export const Login = baseUrl + "login";
+export const LaporUang = baseUrl + 'laporan_keuangan'
+export const Login = baseUrl + 'login';
 //O
-export const GetOpex = baseUrl + "opex"
-export const AddOpex = baseUrl + "opex_code"
+export const GetOpex = baseUrl + 'opex'
+export const AddOpex = baseUrl + 'opex_code'
 //P
-export const Product = baseUrl + "products"
-export const ProductSearch = baseUrl + "products/search"
-export const ProductCode = baseUrl + "products_code"
+export const Product = baseUrl + 'products'
+export const ProductSearch = baseUrl + 'products/search'
+export const ProductCode = baseUrl + 'products_code'
 
-export const PurchaseOrder = baseUrl + "purchase_orders"
-export const PurchaseOrderMonthly = baseUrl + "purchase_orders/monthly"
-export const PurchaseOrderAdd = baseUrl + "purchase_orders_code"
+export const PurchaseOrder = baseUrl + 'purchase_orders'
+export const PurchaseOrderMonthly = baseUrl + 'purchase_orders/monthly'
+export const PurchaseOrderAdd = baseUrl + 'purchase_orders_code'
 //Q
-export const Quatations = baseUrl + "quatations"
-export const QuatationsAdd = baseUrl + "quatations_code"
+export const Quatations = baseUrl + 'quatations'
+export const QuatationsAdd = baseUrl + 'quatations_code'
 //S
-export const SalesOrders = baseUrl + "sales_orders"
-export const SalesOrdersMonthly = baseUrl + "sales_orders/monthly"
-export const SalesOrderAdd = baseUrl + "sales_orders_code"
+export const SalesOrders = baseUrl + 'sales_orders'
+export const SalesOrdersMonthly = baseUrl + 'sales_orders/monthly'
+export const SalesOrderAdd = baseUrl + 'sales_orders_code'
 //T
-export const Tandater = baseUrl + "tandater";
-export const TandaterAdd = baseUrl + "addTandater";
+export const Tandater = baseUrl + 'tandater';
+export const TandaterAdd = baseUrl + 'addTandater';
 //v
-export const Vendor = baseUrl + "vendors"
-export const AddVendor = baseUrl + "vendors_code"
+export const Vendor = baseUrl + 'vendors'
+export const AddVendor = baseUrl + 'vendors_code'
 
-export const GetFakturPajak = baseUrl + "faktur-pajak"
-export const AddFakturPajak = baseUrl + "faktur-pajak-code"
+export const GetFakturPajak = baseUrl + 'faktur-pajak'
+export const AddFakturPajak = baseUrl + 'faktur-pajak-code'
